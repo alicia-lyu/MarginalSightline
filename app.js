a3cf83cb6e04e65942720b2df27a4a651c6ce305
karenlyu21 <pkulyuwenhui@gmail.com>
2022-08-11 15:36:29 +0800

    front-end setup & research page

--- app.js
+++ app.js
@@ -7,11 +7,13 @@ const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const morgan = require('morgan');
 const methodOverride = require('method-override');
+const favicon = require('serve-favicon');
 const Bodypart = require('./models/Bodypart');
 
 mongoose.connect('mongodb://localhost:27017/marginal-sightline');
 
 const app = express()
+app.use(favicon(path.join(__dirname, 'assets/img/thumbnails/favicon.ico')));
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
@@ -26,7 +28,6 @@ app.use(express.static(path.join(__dirname, 'assets')))
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(morgan('tiny'));
-
 
 app.get('/', async (req, res) => {
     const bodyparts = await Bodypart.find({});
